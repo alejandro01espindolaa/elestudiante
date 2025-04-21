02fcccecea5450800b7004d5cc447446dd88bc51
Osbaldo <osbaldo.are2000@gmail.com>
2025-04-21 10:22:57 -0600

    organizacion archivos

--- contacto.js
+++
@@ -1,7 +0,0 @@
-// Script para el formulario de reservación
-document.getElementById('reservationForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('¡Gracias por tu reservación! Te contactaremos pronto para confirmar los detalles.');
-    this.reset();
-});
-
